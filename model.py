e2867953249de5b63336936158a6ef65730aaa4d
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2022-11-04 14:09:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 
-class Model(nn.Module):
+class NNModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
-        super(Model, self).__init__()
+        super(NNModel, self).__init__()
         self.layer1 = nn.Linear(input_size, hidden_size)
         self.layer2 = nn.Linear(hidden_size, hidden_size)
         self.layer3 = nn.Linear(hidden_size, num_classes)
@@ -15,4 +15,3 @@ class Model(nn.Module):
         out = self.relu(out)
         out = self.layer3(out)
         return out
-
